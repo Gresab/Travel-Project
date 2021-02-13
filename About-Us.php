4277651f2a04f89066f73c93f45c688fcd5daaf4
fjollagashi <78988113+fjollagashi@users.noreply.github.com>
2021-02-14 00:45:06 +0100

    Add files via upload

---
+++ About-Us.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Us</title>
+  <link rel="stylesheet" href="css/style2.css">
+  <link rel="stylesheet" href="css/bar.css">
+
+</head>
+
+<body>
+
+  <nav class="header">
+    <div class="brand-title">The Mountains</div>
+    <a href="#" class="toggle-button">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </a>
+    <div class="navbar-links">
+      <ul>
+        <li><a href="index1.php">Home</a></li>
+        <li><a href="About-Us.php">About us</a></li>
+        <li><a href="Places.php">Places</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href=zzzzz.php">Sign up</a></li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <section>
+    <div class="container">
+      <h1>About Us</h1>
+      <h2>Our Story</h2>
+
+      <p>The Mountains is a tour agency, specializing in organizing tours to all destinations. We are happy to make your
+        holidays unforgettable and amazing!
+        Our agents are experts in finding you the most comfortable and interesting place for you to have breathtaking
+        holidays.
+        Select the country you’d like to visit and provide our agents with estimated time – they’ll find and offer the
+        most suitable tours and hotels.<br>
+        During our work we organized countless journeys for our clients. We started as a small tour bureau, and soon we
+        expanded our offers list.
+        Today we have valuable experience travelling and we can advise the most stunning resorts, cities and countries
+        to visit!<br><br>
+
+        “Remember that happiness is a way of travel, not a destination.”
+
+      </p>
+
+    </div>
+  </section>
+</body>
+
+</html>
