2a4c51a4b1f6bd0cd75035a53446048833fe0c17
fjollagashi <78988113+fjollagashi@users.noreply.github.com>
2021-02-25 00:20:24 +0100

    II

---
+++ businessLogic/adminClass.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'personClass.php';
+
+
+class Admin extends Person
+{
+    public function __construct($username, $password, $role) 
+    {
+        parent::__construct($username, $password, $role); 
+    }
+
+
+    public function setSession()
+    {
+
+        $_SESSION["role"] = 1;
+        $_SESSION['roleName'] = "Administrator";
+    }
+
+    public function setCookie()
+    {
+        setcookie("username", $this->getUsername(), time() + 2 * 24 * 60 * 60);
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
