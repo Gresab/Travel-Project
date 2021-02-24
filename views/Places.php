49e312c1dbf07b79d1d3e74f3c53a8e3bd82f523
fjollagashi <78988113+fjollagashi@users.noreply.github.com>
2021-02-25 00:24:43 +0100

    II

---
+++ views/Places.php
@@ -0,0 +1,103 @@
+<?php
+
+include '../components/header.php'; ?>
+<link rel="stylesheet" href="../css/style3.css" />
+
+    <div class="container">
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/burjkhalifa-big.jpg" alt="">
+            </div>
+
+            <div class="details">
+                <div class="content">
+                    <h1>Burj Khalifa</h1>
+                    <p>Dubai,United Arab Emirates</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/xparis_eiffeltower_ssk500.jpeg.pagespeed.ic.03Zne2dW2F.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>The Eiffel Tower</h1>
+                    <p>Paris</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/machu-Picchu.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>Machu Picchu</h1>
+                    <p>Peruvian Andes</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/great_wall_of_china.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>Great wall of China</h1>
+                    <p>China</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/A-classic-view-of-Taj-Mahal-India.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>Taj Mahala</h1>
+                    <p>India</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/Ha_Long_Bay_in_2019.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>Ha Long Bay</h1>
+                    <p>Vietnam</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/Colosseum-1-1.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>Colosseum</h1>
+                    <p>Italy</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="img">
+                <img src="../fotot/General-view-of-pyramids-from-the-Giza-Plateau.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h1>Pyraminds of Giza</h1>
+                    <p>Egypt</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    </body>
+<script src="../js/main.js"></script>
+</html>
